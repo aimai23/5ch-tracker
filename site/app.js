f960c74c9dd9344160d66144a8d72056757b7a6b
aimai23 <aimai23@users.noreply.github.com>
2026-01-23 22:35:30 +0900

    Update fixes

--- site/app.js
+++ site/app.js
@@ -395,7 +395,7 @@ async function main() {
         trendHtml = `<span class="trend-new">NEW</span>`;
       } else if (item.rank_delta !== undefined && item.rank_delta !== 0) {
         const isUp = item.rank_delta > 0;
-        const arrow = isUp ? "▲" : "▼";
+        const arrow = isUp ? "∧" : "∨";
         const colorClass = isUp ? "trend-up" : "trend-down";
         trendHtml = `<span class="${colorClass}">${arrow}</span>`;
       } else {
